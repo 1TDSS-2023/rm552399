22e96201854ee18d06e2d726ec7adf017679ebe3
Victor Fusco Martins <105394403+Victor-FM@users.noreply.github.com>
2023-04-28 20:55:34 -0300

    Subindo primeira aula de JavaScript - 28/04

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// "use strict"
+
+// var nr1 = "10";
+// var nr2 = "5"; 
+
+
+// console.log(parseInt(nr1) + parseInt(nr1));
+
+// var nr3;
+
+var nome1 = "Dubas";
+// let  nome2 = "Serbatinho";
+// const nome3 = "Julia";
+
+if(nome1 == ""){
+    var nome1 = "Julia"
+}
+
+console.log();
